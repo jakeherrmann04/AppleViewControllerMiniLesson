287b593b672720cf7b1fff2ead93a0ba106e5ede
jakeherrmann04 <jake.herrmann04@gmail.com>
2015-11-20 10:59:17 -0700

    Finished Project

--- AppleViewControllersMiniLesson/ViewController.swift
+++ AppleViewControllersMiniLesson/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MessageUI
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cameraButtnClicked(sender: AnyObject) {
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        
+        let alert = UIAlertController(title: "Select Photo Option", message: nil, preferredStyle: .ActionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary){
+            alert.addAction(UIAlertAction(title: "Camera Roll", style: .Default, handler: { (_) -> Void in
+                
+                imagePicker.sourceType = .PhotoLibrary
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+            }))
+        
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.SavedPhotosAlbum){
+            alert.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { (_) -> Void in
+                
+                imagePicker.sourceType = .Camera
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+            }))
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
 
+    @IBAction func mailButtonClicked(sender: AnyObject) {
+       
+        if(MFMailComposeViewController.canSendMail()){
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.mailComposeDelegate = self
+
+            mailComposer.setSubject("Hey Mike")
+            mailComposer.setMessageBody("I hope this snow is treating you right", isHTML: false)
+            
+            self.presentViewController(mailComposer, animated: true, completion: nil)
+
+        }
+        
+        
+    }
+        
+    
+    
+    @IBAction func activityButtonClicked(sender: AnyObject) {
+        let activity = UIActivityViewController(activityItems: [UIActivityTypeMail, UIActivityTypeMessage], applicationActivities: [])
+     
+        
+        self.presentViewController(activity, animated: true, completion: nil)
+        
+    }
+    @IBAction func webButtonClicked(sender: AnyObject) {
+
+        let svc = SFSafariViewController(URL: NSURL(string: "https://www.facebook.com/michaeliscool?fref=ts")!)
+        self.presentViewController(svc, animated: true, completion: nil)
+        
+    }
 }
 
